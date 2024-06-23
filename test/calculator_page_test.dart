029300d682b34217a6239866b0be4051435f04c9
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2024-06-24 00:49:43 +0700

    Update calculator page test run

--- test/calculator_page_test.dart
+++ test/calculator_page_test.dart
@@ -1,3 +1,4 @@
+import 'package:smath/common/consts/component_key.dart';
 import 'package:smath/common/utils/route_util.dart';
 import 'package:smath/common/utils/theme_util.dart';
 import 'package:smath/presentation/bloc/calculator/calculator_cubit.dart';
@@ -48,7 +49,7 @@ void main() {
       child: BlocBuilder<GlobalBloc, GlobalState>(
         builder: (context, state) {
           return MaterialApp(
-            title: 'Simple Calculator',
+            title: 'Smath',
             debugShowCheckedModeBanner: false,
             themeMode: state.themeMode,
             theme: state.themeUtil.lightTheme,
@@ -61,11 +62,20 @@ void main() {
   }
 
   group(
-    'CalculatorPage ',
+    'CalculatorPage',
     () {
       testWidgets(
-        'CalculatorKeyboard should render calculator buttons',
+        'CalculatorKeyboard should render calculator buttons & interact',
         (WidgetTester tester) async {
+          /// This code will dictate widget test screen size, height and width
+          /// for testing. The default it will run on smaller screen size that
+          /// can lead to failed testing. Which is not normal.
+          // ignore: deprecated_member_use
+          tester.binding.window.physicalSizeTestValue = const Size(
+            430,
+            1500,
+          );
+
           // Build the widget within a BlocProvider.
           await pumpCalculatorPage(tester);
 
@@ -105,78 +115,144 @@ void main() {
             findsOneWidget,
           );
 
+          /// Check if there is any 1 button
           expect(
             find.text('1'),
             findsOneWidget,
           );
 
+          /// Check if there is any 2 button
           expect(
             find.text('2'),
             findsOneWidget,
           );
 
+          /// Check if there is any 3 button
           expect(
             find.text('3'),
             findsOneWidget,
           );
 
+          /// Check if there is any 4 button
           expect(
             find.text('4'),
             findsOneWidget,
           );
+
+          /// Check if there is any 5 button
           expect(
             find.text('5'),
             findsOneWidget,
           );
 
+          /// Check if there is any 6 button
           expect(
             find.text('6'),
             findsOneWidget,
           );
 
+          /// Check if there is any 7 button
           expect(
             find.text('7'),
             findsOneWidget,
           );
 
+          /// Check if there is any 8 button
           expect(
             find.text('8'),
             findsOneWidget,
           );
 
+          /// Check if there is any 9 button
           expect(
             find.text('9'),
             findsOneWidget,
           );
 
+          /// Check if there is any 0 button
           expect(
             find.text('0'),
             findsOneWidget,
           );
 
+          /// Check if there is any . button
           expect(
             find.text('.'),
+            findsOneWidget,
+          );
+
+          /// Optionally, we can tap the buttons to verify interactions.
+          /// simulating 12 + 5.5
+          /// Tap 1
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonOne,
+              ),
+            ),
+          );
+
+          /// Tap 2
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonTwo,
+              ),
+            ),
+          );
+
+          /// Tap +
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonAdd,
+              ),
+            ),
+          );
+
+          /// Tap 5
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonFive,
+              ),
+            ),
+          );
+
+          /// Tap .
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonDot,
+              ),
+            ),
+          );
+
+          /// Tap 5
+          await tester.tap(
+            find.byKey(
+              const Key(
+                ComponentKey.buttonFive,
+              ),
+            ),
+          );
+
+          /// Do pump, in case there is animation
+          await tester.pumpAndSettle();
+
+          /// Check if the current calculation is match
+          expect(
+            find.text('12+5.5'),
+            findsOneWidget,
+          );
+
+          /// Check if the current result is match
+          expect(
+            find.text('17.5'),
             findsOneWidget,
           );
         },
       );
     },
   );
-}
-
-class NoKeyboardBinding extends WidgetsFlutterBinding
-    with TestDefaultBinaryMessengerBinding {
-  @override
-  void initInstances() {
-    super.initInstances();
-
-    // Override the default text input controller
-    defaultBinaryMessenger.setMockMessageHandler(
-      'flutter/textinput',
-      (message) async {
-        // Return an empty response to prevent keyboard actions
-        return null;
-      },
-    );
-  }
 }
